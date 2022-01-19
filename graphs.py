debdd9ea9484db197275f1718036916bef9614dd
abhimanyuu09 <97113844+abhimanyuu09@users.noreply.github.com>
2022-01-19 21:20:41 +0530

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -3,6 +3,6 @@ import plotly.express as px
 
 df = pd.read_csv("data.csv")
 
-fig = px.scatter(df,x="date",y="cases",color="country)
+fig = px.scatter(df,x="date",y="cases",color="country")
                  
 fig.show()
